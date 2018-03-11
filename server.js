915dd7068cd1b80697d4848f96c9bb26696aa7de
Den <den@vdsinside.com>
2018-03-11 20:42:38 +0200

    - fixed

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var longpoll = require("express-longpoll")(app);
  
-// Creates app.get("/poll") for the long poll 
 longpoll.create("/poll");
 
 app.get("/", function(req, res) {
@@ -19,17 +18,18 @@ app.listen(8080, function() {
     console.log("Listening on port 8080");
 });
  
-// Publishes data to all clients long polling /poll endpoint 
-// You need to call this AFTER you make a GET request to /poll 
-//longpoll.publish("/poll", { text: 'started.' });
- 
 var dgram = require('dgram')
 var socket = dgram.createSocket('udp4')
 
-socket.on('message', function(msg) {
-	t = msg.toString();
-	console.log('- publishing: ' + t);
-	longpoll.publish("/poll", { text: t });
+socket.on('message', function(msg, rinfo) {
+	console.log('-'+ rinfo.address +':'+ rinfo.port +' ['+ rinfo.size +'] => ' + msg.toString());
+	longpoll.publish("/poll", { 
+		src_addr: rinfo.address,
+		src_port: rinfo.port,
+		size: rinfo.size,
+		msg: msg.toString(),
+		data: msg,
+	});
 });
 
 socket.bind(5514, '127.0.0.1');
